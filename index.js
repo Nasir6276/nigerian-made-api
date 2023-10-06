342578337666895faaf3f83973f8fb222e6ff4e3
Nasir6276 <ni3262019@gmail.com>
2023-10-06 20:45:41 +0100

    update

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ app.get('/', (req, res) => {
 app.use('/api', buyerRoute)
 app.use('/api', sellerRoute)
 
-app.listen('3.10.19.255', () => {
+app.listen('80', () => {
     console.log('backend is running.');
 })
